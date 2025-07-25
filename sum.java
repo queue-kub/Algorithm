790266f73b4d1322de2240d3555cb1dd64bc0d35
“queue-kub” <684259032@webmail.npru.ac.th>
2025-07-25 14:42:59 +0700

    add sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+
+        int num1 = 4;
+        int num2 = 6;
+        int sum = num1 + num2;
+
+        System.out.println ("sum of "+num1+" and "+num2+" is = "+sum);
+    }
+}
